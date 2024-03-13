803355596c8d970ec3de076f994b68193b40931e
IgorMoura1 <igor.moura@sptech.school>
2024-03-12 23:05:12 -0300

    Iniciando o Login no Java

---
+++ LoginBancoDeDados.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class LoginBancoDeDados {
+    public static void main(String[] args) {
+        Scanner scannerUsuario = new Scanner(System.in);
+        System.out.println("Digite o seu usuário");
+        String entradaUsuario = scannerUsuario.next();
+
+        System.out.println("Digite sua senha");
+        
+    }
+}
